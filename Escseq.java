8a0c071759b52fc14a9950277416fa202ee2d0e7
vandal1er <165736672+vandal1er@users.noreply.github.com>
2024-08-30 05:41:46 +0000

    done

---
+++ Escseq.java
@@ -0,0 +1,6 @@
+public class Escseq {
+    public static void main(String args[])
+    {
+        System.out.println("7\n\t14\n21\n\t28\n35");
+    }
+}
---
+++ Tempconvert.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tempconvert {
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Celsius value: ");
+        float celsius = scanner.nextFloat();
+
+        float fahr = (celsius * (9f / 5f)) + 32f;
+        System.out.println(String.format("%.2f Celsius is %.2f Fahrenheit", celsius, fahr));
+        scanner.close();
+    }
+}
