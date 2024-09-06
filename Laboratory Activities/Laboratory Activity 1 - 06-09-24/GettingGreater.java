c1bcfc31404e4786c6395c872a3aa6336eac8fd9
vandal1er <165736672+vandal1er@users.noreply.github.com>
2024-09-06 14:00:56 +0800

    Create GettingGreater.java

---
+++ Laboratory Activities/Laboratory Activity 1 - 06-09-24/GettingGreater.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GettingGreater 
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Getting the Greater Value");
+		System.out.print("Enter the first character: ");
+		char a = input.next().charAt(0);
+		System.out.print("Enter the second character: ");
+		char b = input.next().charAt(0);
+		
+		char c = (char) Math.max(a, b);
+		
+		System.out.println("--------------------------------------------");	
+		System.out.println("The character with greater value is "+ c);
+		System.out.println("--------------------------------------------");	
+		System.out.println("Showing the ASCII Codes");
+		System.out.println(a + " : " + (int)a);
+		System.out.println(b + " : " + (int)b);
+		
+		scanner.close()
+	}
+}
