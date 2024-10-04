eeaa56783dbc5f1ea624815e66c4b6a0555d4e8d
vandal1er <165736672+vandal1er@users.noreply.github.com>
2024-10-04 21:43:47 +0800

    Create GettingArrayMean.java

---
+++ Laboratory Activities/Laboratory Activity 2 - 04-10-24/GettingArrayMean.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class GetArrayMean {
+	
+	public static float getArrayMean(float[] array)
+	{
+		float sum = 0f;
+		for (float i : array)
+		{
+			sum += i;
+		}
+		
+		float result = sum/array.length;
+		
+		return result;
+	}
+	
+	public static void main(String[] args) 
+	{
+		int size = 0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter number of elements: ");
+		size = input.nextInt();
+		
+		float[] arr = new float[size];
+		
+		for (int i = 0; i < size; i++)
+		{
+			System.out.print("Enter number: ");
+			arr[i] = input.nextFloat();
+		}
+		
+		System.out.println("Mean of array is: " +getArrayMean(arr));
+		input.close();
+	}
+}
